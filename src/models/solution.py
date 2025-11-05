9136650c754bfff102d76467636e03341dd47946
Y4ngfr <yangrodrigues.aluno@unipampa.edu.br>
2025-11-05 20:03:59 -0300

    criação classe toy e representação da solução

---
+++ src/models/solution.py
@@ -0,0 +1,34 @@
+from typing import List
+from .toy import Toy
+
+class Solution:
+    """Classe que representa uma solução para o problema UKP"""
+    
+    def __init__(self, toys: List[Toy], quantities: List[int] = None):
+        self.toys = toys    # todos os tipos de brinquedos
+
+        if quantities:      # quantidades de cada tipo de brinquedo
+            self.quantities = quantities
+        else:
+            self.quantities = [0] * len(toys)   # [0, 0, ..., 0] com len(toys) posições
+    
+    def total_cost(self) -> float:
+        """Calcula o custo total da solução"""
+        sum = 0
+        for toy, qty in zip(self.toys, self.quantities):
+            sum += toy.production_cost * qty
+        return sum
+    
+    def total_profit(self) -> float:
+        """Calcula o lucro total da solução"""
+        sum = 0
+        for toy, qty in zip(self.toys, self.quantities):
+            sum += toy.production_cost * qty
+        return sum
+    
+    def is_valid(self, budget: float) -> bool:
+        """Verifica se a solução respeita o orçamento"""
+        return self.total_cost() <= budget
+    
+    def __repr__(self):
+        return f"Solution(cost={self.total_cost()}, profit={self.total_profit()}, quantities={self.quantities})"
---
+++ src/models/toy.py
@@ -0,0 +1,16 @@
+class Toy:
+    """Classe que representa um tipo de brinquedo"""
+    
+    def __init__(self, id: int, name: str, production_cost: float, sale_price: float):
+        self.id = id
+        self.name = name
+        self.production_cost = production_cost  # custo de produção
+        self.sale_price = sale_price            # preço do produto na venda
+    
+    def profit(self) -> float:
+        """Calcula o lucro do brinquedo"""
+        return self.sale_price - self.production_cost   # preço - custo
+    
+    def __repr__(self):
+        """Função que define como o objeto será printado como string"""
+        return f"Toy(id={self.id}, name='{self.name}', cost={self.production_cost}, price={self.sale_price}, profit={self.profit})"
