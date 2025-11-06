f5db1d2d76088d862d3a411e6213825ab19968d4
Y4ngfr <yangrodrigues.aluno@unipampa.edu.br>
2025-11-06 18:34:04 -0300

    ideia inicial de algoritmo genetico

---
+++ src/algorithms/genetic_algorithm.py
@@ -0,0 +1,18 @@
+#   Inicializar população com soluções aleatórias
+#
+#
+#   Cálculo da função fitnes: lucro total da solução
+#   Pode incluir penalização proporcional ao excesso de soluções inválidas
+#
+#
+#   Critério de parada: conclusão de todas as gerações
+# 
+# 
+#   Seleção dos melhores cromossomos (soluções) através da seleção por torneio
+#
+#
+#   Crossover de um ponto (single-point-crossover) com uma probablidade de ocorrer
+#   e seleção de genes aleatória
+#
+#
+#   Mutação aleatória de algumas quantidades
